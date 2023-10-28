4024ea26c22cb2c93dbfd62f4f40f44e8b7ad595
Sagonzalezn <sagonzalezn@unal.edu.co>
2023-10-28 13:17:34 -0500

    Creación de rutas, css global y escenas

---
+++ src/state/index.js
@@ -0,0 +1,47 @@
+import { create } from "@mui/material/styles/createTransitions";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {//TIENE LA "s" al final del archivo?
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light"
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setRelated: (state, action) => {
+            if (state.user) {
+                state.user.related = action.payload.related;
+            } else {
+                console.error("user related non-existent :(");
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
+        }
+    }
+})
+
+export const { setMode, setLogin, setLogout, setRelated, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
